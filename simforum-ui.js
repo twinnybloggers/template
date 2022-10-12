68b72db9bccef0f72a2e9b5fd9c2e7b681b3ff1d
Tokko Blogger <21377149+twinnybloggers@users.noreply.github.com>
2022-10-12 19:24:02 +0700

    Create simforum-ui.js

---
+++ simforum-ui.js
@@ -0,0 +1,18 @@
+/* ================================================================================================
+
+    Peringatan !!
+
+===================================================================================================
+
+    Mengubah / Menghapus file javascript (js) ini dapat mengakibatkan :
+
+    1. Kerusakan / error pada template
+    2. Menghilangkan fungsi update / perbaikan otomatis pada template
+    3. Tidak akan mendapatkan support/bantuan dari kreator template
+
+    Terima kasih sudah menggunakan Template Dari Tokko Blogger. :)
+
+    Salam,
+    Tokko Blogger - Blog White Store
+    New Path : simforumui-blogger-template-forum
+================================================================================================ */
