376279f1d58e47c1f79b61c1a297290bb9dae0ae
Helmy Kambuna Otaya (Tokko Blogger) <21377149+twinnybloggers@users.noreply.github.com>
2023-06-10 20:54:58 +0700

    Create TokkoInvoiceModule.js

---
+++ TokkoInvoiceModule.js
@@ -0,0 +1,131 @@
+function _0x33e4() {
+    var _0x3ada67 = ['html', '<tr><td>', 'substr', ' x ', 'val', 'data-copy', '<br/><fieldset class=\'note\' style=\'padding:15px 20px 20px;border:1px solid var(--key);border-radius:var(--radius);\'><legend style=\'padding:0 10px;background:var(--silver);color:var(--key);\'><i class=\'icon left\'><svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 512 512\'><path d=\'M404.3 86l-202 202c-1.5 1.5-2.3 3.5-2.3 5.6v26.5c0 4.4 3.6 7.9 7.9 7.9h26.3c2.1 0 4.2-.8 5.7-2.3l202.1-202c3.1-3.1 3.1-8.1 0-11.2L415.5 86c-3.1-3.1-8.1-3.1-11.2 0zM475.6 67l-14.4-14.4-.2-.2c-3.1-2.7-7.2-4.4-11.5-4.4-4.4 0-8.5 1.7-11.6 4.5l-11.3 11.4c-1.5 1.6-1.5 4.1 0 5.6L437 79.9l21.7 21.7c1.6 1.6 4.1 1.6 5.7 0l11.3-11.3c2.8-3.1 4.4-7.1 4.4-11.6-.1-4.4-1.7-8.6-4.5-11.7z\'/><g><path d=\'M250 342c-3 3-7.1 4.7-11.3 4.7H197.3c-8.8 0-16-7.2-16-16V289.2c0-4.2 1.7-8.3 4.7-11.3l.8-.8 147.6-147.6c2.5-2.5.7-6.8-2.8-6.8H90.7C58.3 122.7 32 149 32 181.4v224c0 32.4 26.3 58.7 58.7 58.7h256c32.4 0 58.7-26.3 58.7-58.7v-209c0-3.6-4.3-5.3-6.8-2.8L250.8 341.2l-.8.8z\'/></g></svg></i> <b>Catatan :</b></legend><p>', '23584SFbKEG', '931239ukwlLS', '\" title=\"', '.post-body table .client_info', 'length', '4IIdjNP', 'form', 'body', '<b>Kode Lisensi</b> hanya bisa digunakan pada website dengan domain : <b>', ' ( ', 'img', 'td:nth-child(2)', 'number', '.post-body table .note', '</b><br><br><small>#ERR_DOMAIN</small>', '<tr>', 'reverse', 'hostname', 'onload', 'Kirim via WhatsApp?', 'Rp ', '11wchFKC', 'join', '</b></td>', '#detail', '.post-body table tbody.item tr', 'href', '<input id=\"copy\" type=\"text\" value=\"', 'discount', 'preventDefault', 'each', '282588KYFByh', 'name', 'Error share', 'td:nth-child(1)', '<small>', 'toggle', 'Successful share', 'charAt', 'undefined', 'copy', 'open', '<td><b>', '</small></td>', ' - ', 'slice', 'Developed by. https://kangrian.net ( Premium Version )', 'canonical', '#copy', 'submit', 'toggleClass', '.cta', ' <span class=\"btn small right\" data-copy=\"', '</b><br/><small>', '#invoice', '\"/>       <div><div><b style=\"display:block;margin-bottom:10px;\">', '</td><td><b>- ', '</small></div></div>        <div><input type=\"radio\" name=\"payment\" value=\"', 'Dilarang menghapus variable <b>$license_item</b>..', '<td>', '2183426AYthLj', '#invoice .client .info', '</p>                    <br>                    <hr style=\"border:0;border-top:1px solid #ddd;\">                    <br>                    <a href=\"https://lapak.kangrian.net/', 'replaceAll', '<h3><i class=\"icon left\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><circle cx=\"416\" cy=\"152\" r=\"48\"/><path d=\"M416 218.5c-5.4 0-10.6-.7-15.6-1.9L353 264.9c-.8.8-.8 2 0 2.8l75.3 80.2c5.1 5.1 5.1 13.3 0 18.4-2.5 2.5-5.9 3.8-9.2 3.8s-6.7-1.3-9.2-3.8l-75-79.9c-.8-.8-2.1-.8-2.9 0L313.7 305c-15.3 15.5-35.6 24.1-57.4 24.2-22.1.1-43.1-9.2-58.6-24.9l-17.6-17.9c-.8-.8-2.1-.8-2.9 0l-75 79.9c-2.5 2.5-5.9 3.8-9.2 3.8s-6.7-1.3-9.2-3.8c-5.1-5.1-5.1-13.3 0-18.4l75.3-80.2c.7-.8.7-2 0-2.8L51.4 155.3c-1.3-1.3-3.4-.4-3.4 1.4V376c0 17.6 14.4 32 32 32h352c17.6 0 32-14.4 32-32V198c-12.1 12.6-29.1 20.5-48 20.5z\"/><path d=\"M349.5 152c0-11.6 3-22.5 8.2-32H79.9c-7.5 0-14.4 2.6-19.8 7L217 286.7c10.4 10.6 24.3 16.4 39.1 16.4s28.7-5.8 39.1-16.4l80.5-81.9c-16-12.2-26.2-31.3-26.2-52.8z\"/></svg></i>', '    <div id=\"notice\">        <p></p>    </div>    ', 'attr', 'toUpperCase', 'grand_total', '</td>', 'remove', '#invoice tfoot', '#notice', '</small>', 'indexOf', '\" style=\"position:fixed;bottom:0;right:0;\">', '</tr>', '#invoice .total h3', '#invoice .pay-toggle', 'page', '315PVLlYu', '</p></fieldset>', 'replace', '#form', '            \x0a\x0a            via. ', '</b>', 'payment', '.blogger.com', 'whatsapp', '<b>Kode Lisensi</b> dibutuhkan..<br><br><small>#ERR_LICENSE</small>', 'copied', '[invoice_title]', '[invoice_payment]', '<b>', 'Developed by. https://lapak.kangrian.net ( Free / Trial Version )', 'append', '\" title=\"No. Rekening / Tujuan\">', '11258RNuilf', '</b> ', 'execCommand', 'scrollTop', 'invoice-blogger-template', '.post-body table .client_name', '73GfHIfx', 'catch', 'title', '644530NSKHYw', '            <link href=\"https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap\" rel=\"stylesheet\">            <style>hr { margin:0!important; } body { padding: 0px!important; } * { font-family: \"Rubik\", sans-serif; font-weight:400; box-sizing:border-box;margin:0;padding:0; text-decoration:none; color:#636363; } b { font-weight: 500; color:#232323; }</style>            <div id=\"notice_html\" style=\"display:flex;background:#fafafa;min-height:100vh;text-align:center;\">                <div style=\"margin:auto;width:480px;max-width:80%;background:white;padding:30px;border-radius:10px;border:1px solid #ddd;\">                    <svg style=\"fill:#636363;width:100px;height:100px;display:block;margin:0 auto 20px\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M228.9 79.9L51.8 403.1C40.6 423.3 55.5 448 78.9 448h354.3c23.3 0 38.2-24.7 27.1-44.9L283.1 79.9c-11.7-21.2-42.5-21.2-54.2 0zM273.6 214L270 336h-28l-3.6-122h35.2zM256 402.4c-10.7 0-19.1-8.1-19.1-18.4s8.4-18.4 19.1-18.4 19.1 8.1 19.1 18.4-8.4 18.4-19.1 18.4z\"/></svg>                    <h2><b>Peringatan</b> :</h2>                    <br>                    <p>', 'split', 'XXX', 'includes', 'pay_submit_text', 'addClass', '</span>', '6iopiGL', '</span><br/><small>A/n. ', 'src', 'removeClass', 'localhost', 'clearTimeout', 'select', 'share', '* Fitur share tidak tersedia pada mode web-view..', '3156080zSanIh', 'text', 'click', 'subtotal', 'log', '#invoice .payment', '<b>Kode Lisensi</b> yang digunakan tidak valid.</b><br><br><small>#ERR_KEY</small>', 'url', '<b>Kode Lisensi</b> hanya bisa digunakan untuk <b>'];
+    _0x33e4 = function () {
+        return _0x3ada67;
+    };
+    return _0x33e4();
+}
+var _0x27d3ce = _0x22ea;
+(function (_0x1516d9, _0x11f6ee) {
+    var _0x242634 = _0x22ea,
+        _0x412371 = _0x1516d9();
+    while (!![]) {
+        try {
+            var _0x346803 = parseInt(_0x242634(0x1be)) / 0x1 * (parseInt(_0x242634(0x243)) / 0x2) + parseInt(_0x242634(0x1e3)) / 0x3 * (parseInt(_0x242634(0x1e7)) / 0x4) + -parseInt(_0x242634(0x1d2)) / 0x5 * (parseInt(_0x242634(0x1c9)) / 0x6) + parseInt(_0x242634(0x21e)) / 0x7 + parseInt(_0x242634(0x1e2)) / 0x8 * (-parseInt(_0x242634(0x232)) / 0x9) + parseInt(_0x242634(0x1c1)) / 0xa + -parseInt(_0x242634(0x1f7)) / 0xb * (parseInt(_0x242634(0x201)) / 0xc);
+            if (_0x346803 === _0x11f6ee) break;
+            else _0x412371['push'](_0x412371['shift']());
+        } catch (_0x193050) {
+            _0x412371['push'](_0x412371['shift']());
+        }
+    }
+}(_0x33e4, 0x52f2c));
+const $license_item = _0x27d3ce(0x1bc);
+license();
+
+
+window[_0x27d3ce(0x1f4)] = function () {
+    var _0x54ec3b = _0x27d3ce;
+    $('img#logo')[_0x54ec3b(0x224)](_0x54ec3b(0x1cb), $_config[_0x54ec3b(0x231)]['logo']);
+    var _0xa4112c = $(_0x54ec3b(0x1bd))[_0x54ec3b(0x1d3)]()[_0x54ec3b(0x234)](/  |\r\n|\n|\r/gm, '');
+    $(_0x54ec3b(0x21f))[_0x54ec3b(0x241)](_0x54ec3b(0x222) + _0xa4112c + '</h3>');
+    var _0x3b02fa = $(_0x54ec3b(0x1e5))[_0x54ec3b(0x1db)]();
+    $(_0x54ec3b(0x21f))[_0x54ec3b(0x241)](_0x54ec3b(0x205) + _0x3b02fa + _0x54ec3b(0x22b));
+    var _0x30e5a2 = 0x0,
+        _0x57b2ed = 0x0;
+    $(_0x54ec3b(0x1fb))[_0x54ec3b(0x200)](function () {
+        var _0x4741b7 = _0x54ec3b,
+            _0x22b9f4 = $(_0x4741b7(0x204), this)[_0x4741b7(0x1d3)]()[_0x4741b7(0x234)](/  |\r\n|\n|\r/gm, ''),
+            _0x53a28c = $(_0x4741b7(0x1ed), this)[_0x4741b7(0x1d3)]()['replace'](/  |\r\n|\n|\r/gm, '')[_0x4741b7(0x221)]('.', ''),
+            _0x3e07d6 = $('td:nth-child(3)', this)[_0x4741b7(0x1d3)]()['replace'](/  |\r\n|\n|\r/gm, ''),
+            _0x28dbf3 = _0x53a28c * _0x3e07d6[_0x4741b7(0x1c3)](' ')[0x0];
+        if (_0x22b9f4) {
+            var _0x59ec86 = _0x4741b7(0x1f1);
+            _0x59ec86 += _0x4741b7(0x20c) + _0x22b9f4 + _0x4741b7(0x217) + separator(_0x53a28c, _0x4741b7(0x1f6)) + _0x4741b7(0x1de) + _0x3e07d6 + _0x4741b7(0x20d), _0x59ec86 += _0x4741b7(0x21d) + separator(_0x28dbf3, _0x4741b7(0x1f6)) + _0x4741b7(0x227), _0x59ec86 += _0x4741b7(0x22e), $('#invoice tbody')[_0x4741b7(0x241)](_0x59ec86), _0x30e5a2 = _0x30e5a2 + _0x28dbf3;
+        }
+    }), $(_0x54ec3b(0x229))[_0x54ec3b(0x241)]('<tr><td>' + $_config[_0x54ec3b(0x1d3)][_0x54ec3b(0x1d5)] + '</td><td><b>' + separator(_0x30e5a2, _0x54ec3b(0x1f6)) + '</b></td></tr>');
+    var _0x587968 = $('.post-body table .discount_val')['text']()[_0x54ec3b(0x234)](/  |\r\n|\n|\r/gm, '');
+    if (_0x587968) {
+        _0x587968 = _0x587968[_0x54ec3b(0x221)]('.', '');
+        if (_0x587968[_0x54ec3b(0x1c5)]('%')) {
+            _0x587968 = _0x587968['replaceAll']('%', '');
+            var _0x2963df = _0x30e5a2 * _0x587968 / 0x64;
+            $('#invoice tfoot')['append'](_0x54ec3b(0x1dc) + $_config[_0x54ec3b(0x1d3)][_0x54ec3b(0x1fe)] + _0x54ec3b(0x1eb) + _0x587968 + '% )</td><td><b>- ' + separator(_0x2963df, _0x54ec3b(0x1f6)) + _0x54ec3b(0x1f9)), _0x57b2ed = _0x30e5a2 - _0x2963df;
+        } else $(_0x54ec3b(0x229))[_0x54ec3b(0x241)]('<tr><td>' + $_config[_0x54ec3b(0x1d3)][_0x54ec3b(0x1fe)] + _0x54ec3b(0x21a) + separator(_0x587968, 'Rp ') + _0x54ec3b(0x1f9)), _0x57b2ed = _0x30e5a2 - _0x587968;
+    } else _0x57b2ed = _0x30e5a2;
+    var _0x6573f = $('.post-body table .tax_val')[_0x54ec3b(0x1d3)]()[_0x54ec3b(0x234)](/  |\r\n|\n|\r/gm, '');
+    _0x6573f && (_0x6573f = _0x6573f['replaceAll']('%', ''), $tax_count = _0x57b2ed * _0x6573f / 0x64, $(_0x54ec3b(0x229))[_0x54ec3b(0x241)]('<tr><td>' + $_config[_0x54ec3b(0x1d3)]['tax'] + _0x54ec3b(0x1eb) + _0x6573f + '% )</td><td><b>' + separator($tax_count, _0x54ec3b(0x1f6)) + '</b></td>'), _0x57b2ed = _0x57b2ed + $tax_count);
+    $(_0x54ec3b(0x22f))[_0x54ec3b(0x1db)](separator(_0x57b2ed, _0x54ec3b(0x1f6)) + _0x54ec3b(0x216) + _0x57b2ed + _0x54ec3b(0x1e4) + $_config[_0x54ec3b(0x1d3)][_0x54ec3b(0x226)] + '\">' + $_config[_0x54ec3b(0x1d3)][_0x54ec3b(0x20a)] + _0x54ec3b(0x1c8));
+    var _0x5dcfd9 = $(_0x54ec3b(0x1ef))['text']()[_0x54ec3b(0x234)](/  |\r\n|\n|\r/gm, '');
+    _0x5dcfd9 && $('#invoice #detail')[_0x54ec3b(0x241)](_0x54ec3b(0x1e1) + _0x5dcfd9 + _0x54ec3b(0x233));
+    for (i in $_config['payment']) {
+        var _0xb6ca84 = '<label>        <img src=\"' + $_config['payment'][i][_0x54ec3b(0x1ec)] + _0x54ec3b(0x219) + i + _0x54ec3b(0x237) + $_config[_0x54ec3b(0x238)][i][_0x54ec3b(0x1ee)] + _0x54ec3b(0x216) + $_config[_0x54ec3b(0x238)][i][_0x54ec3b(0x1ee)] + _0x54ec3b(0x242) + $_config[_0x54ec3b(0x1d3)][_0x54ec3b(0x20a)] + _0x54ec3b(0x1ca) + $_config['payment'][i]['name'] + _0x54ec3b(0x21b) + i + '\" required></div>    </label>';
+        $(_0x54ec3b(0x1d7))['append'](_0xb6ca84);
+    }
+    $(_0x54ec3b(0x230))['on'](_0x54ec3b(0x1d4), function () {
+        var _0x227a84 = _0x54ec3b;
+        $(this)['closest'](_0x227a84(0x215))[_0x227a84(0x214)]('toggle'), $(_0x227a84(0x1fa))[_0x227a84(0x206)](), $(_0x227a84(0x235))[_0x227a84(0x206)](), $(window)[_0x227a84(0x246)](0x0);
+    }), $(document)['on'](_0x54ec3b(0x1d4), '[data-copy]', function (_0x1ae5f2) {
+        var _0x44e567 = _0x54ec3b,
+            _0x3afaa5 = $(this)[_0x44e567(0x224)](_0x44e567(0x1e0)),
+            _0xe17074 = $(this)[_0x44e567(0x1d3)](),
+            _0x5f019e = $(this)[_0x44e567(0x224)]('title');
+        $(_0x44e567(0x1e9))[_0x44e567(0x241)](_0x44e567(0x1fd) + _0x3afaa5 + _0x44e567(0x22d)), $(_0x44e567(0x212))[_0x44e567(0x1cf)](), document[_0x44e567(0x245)]('copy'), notice(_0x44e567(0x23f) + _0x5f019e + _0x44e567(0x244) + $_config['text'][_0x44e567(0x23c)], 0x1), $('#copy')[_0x44e567(0x228)]();
+    }), $('#invoice form')['on']('click', 'button', function () {
+        var _0x4a6f6b = _0x54ec3b;
+        !$('#invoice form [name=payment]:checked')[_0x4a6f6b(0x1df)]() && $(window)[_0x4a6f6b(0x246)](0x0);
+    }), $(_0x54ec3b(0x218))['on'](_0x54ec3b(0x213), _0x54ec3b(0x1e8), function (_0x16d171) {
+        var _0x20e1c3 = _0x54ec3b;
+        _0x16d171[_0x20e1c3(0x1ff)]();
+        if (confirm(_0x20e1c3(0x1f5))) {
+            var _0x24dc00 = $_config[_0x20e1c3(0x1d3)][_0x20e1c3(0x1c6)][_0x20e1c3(0x221)](_0x20e1c3(0x23d), $_config[_0x20e1c3(0x231)]['title'] + _0x20e1c3(0x20e) + $_config[_0x20e1c3(0x231)][_0x20e1c3(0x202)])[_0x20e1c3(0x221)](_0x20e1c3(0x23e), $('#invoice form [name=payment]:checked')['val']()) + _0x20e1c3(0x236) + $_config[_0x20e1c3(0x1d9)]['canonical'];
+            location[_0x20e1c3(0x1fc)] = 'https://api.whatsapp.com/send?phone=' + $_config[_0x20e1c3(0x23a)] + '&text=' + encodeURIComponent(_0x24dc00)['replaceAll']('%20%20', '');
+        }
+    }), custom_js();
+};
+
+function separator(_0x4cbc68, _0x37f207 = '') {
+    var _0x7ffd0b = _0x27d3ce,
+        _0xaa8267 = '',
+        _0x4221c9 = _0x4cbc68['toString']()[_0x7ffd0b(0x1c3)]('')[_0x7ffd0b(0x1f2)]()['join']('');
+    for (var _0x5a4946 = 0x0; _0x5a4946 < _0x4221c9[_0x7ffd0b(0x1e6)]; _0x5a4946++)
+        if (_0x5a4946 % 0x3 == 0x0) _0xaa8267 += _0x4221c9[_0x7ffd0b(0x1dd)](_0x5a4946, 0x3) + '.';
+    return _0x37f207 ? _0x37f207 + ' ' + _0xaa8267[_0x7ffd0b(0x1c3)]('', _0xaa8267[_0x7ffd0b(0x1e6)] - 0x1)[_0x7ffd0b(0x1f2)]()[_0x7ffd0b(0x1f8)]('') : _0xaa8267[_0x7ffd0b(0x1c3)]('', _0xaa8267[_0x7ffd0b(0x1e6)] - 0x1)['reverse']()[_0x7ffd0b(0x1f8)]('');
+}
+var timeout_notice;
+
+function _0x22ea(_0x5b457d, _0xc3dd77) {
+    var _0x33e492 = _0x33e4();
+    return _0x22ea = function (_0x22eaa0, _0x7509df) {
+        _0x22eaa0 = _0x22eaa0 - 0x1bc;
+        var _0x210ec2 = _0x33e492[_0x22eaa0];
+        return _0x210ec2;
+    }, _0x22ea(_0x5b457d, _0xc3dd77);
+}
+
+function notice(_0xef1d9a, _0x4fdc25 = '', _0xb6ae0b = 0xbb8) {
+    var _0x9c260d = _0x27d3ce;
+    $(_0x9c260d(0x22a))[_0x9c260d(0x228)](), window[_0x9c260d(0x1ce)](timeout_notice);
+    var _0x45bdfa = _0x9c260d(0x223);
+    $(_0x9c260d(0x1e9))[_0x9c260d(0x241)](_0x45bdfa), $('#notice p')[_0x9c260d(0x1db)](_0xef1d9a), setTimeout(function () {
+        var _0x2d84e7 = _0x9c260d;
+        $(_0x2d84e7(0x22a))[_0x2d84e7(0x1c7)](_0x2d84e7(0x20b));
+    }, 0x64), timeout_notice = window['setTimeout'](function () {
+        var _0x4b7177 = _0x9c260d;
+        $(_0x4b7177(0x22a))['removeClass'](_0x4b7177(0x20b)), setTimeout(function () {
+            var _0x3e33c1 = _0x4b7177;
+            $(_0x3e33c1(0x22a))[_0x3e33c1(0x228)]();
+        }, 0x64);
+    }, _0xb6ae0b), $('#notice')['on'](_0x9c260d(0x1d4), function () {
+        var _0xb8cd32 = _0x9c260d;
+        $(this)[_0xb8cd32(0x1cc)](_0xb8cd32(0x20b));
+    });
+}
+
+function share() {
+    var _0x5828f3 = _0x27d3ce;
+    navigator[_0x5828f3(0x1d0)] ? navigator[_0x5828f3(0x1d0)]({
+        'title': $_config[_0x5828f3(0x231)][_0x5828f3(0x1c0)],
+        'text': $_config[_0x5828f3(0x231)][_0x5828f3(0x1c0)] + _0x5828f3(0x20e) + $_config[_0x5828f3(0x231)][_0x5828f3(0x202)],
+        'url': $_config[_0x5828f3(0x1d9)][_0x5828f3(0x211)]
+    })['then'](() => console['log'](_0x5828f3(0x207)))[_0x5828f3(0x1bf)](_0x3162b8 => console[_0x5828f3(0x1d6)](_0x5828f3(0x203), _0x3162b8)) : notice(_0x5828f3(0x1d1));
+}
